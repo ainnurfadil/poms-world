023d6cf345d26bb40b5c0ad9b28020017d3aca8b
Faris Han <farishanugraha@gmail.com>
2022-06-20 20:15:44 +0700

    Fix eslint; Build, export & deploy

--- components/Collection/index.tsx
+++ components/Collection/index.tsx
@@ -1,11 +1,10 @@
-
 export default function Collection() {
-    return(
+    return (
         <section className="text-gray-700 bgcollection w-full lg:pb-32">
             <img
-            alt="title_roadmap" 
-            className="mx-auto w-36 h-14 md:w-60 md:h-20 "
-            src="./img/title_collection.png"/>
+                alt="title_roadmap"
+                className="mx-auto w-36 h-14 md:w-60 md:h-20 "
+                src="./img/title_collection.png" />
             <div className="overflow-y-auto h-auto w-full flex px-5 text-ijo lg:px-20 m-auto py-12 md:py-4 lg:pt-20 lg:pb-4">
                 <img className="-ml-1 md:-ml-0 mx-3 md:mt-0 w-full md:w-1/5" src="./img/col_01.png" alt='image1' title='image1' />
                 <img className="mx-3 md:mt-0 w-full md:w-1/5" src="./img/col_02.png" alt='image2' title='image2' />
@@ -15,6 +14,5 @@ export default function Collection() {
                 <img className="mx-3 md:mt-0 w-full md:w-1/5" src="./img/col_06.png" alt='image6' title='image6' />
             </div>
         </section>
-        
     )
 }
